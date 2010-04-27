adfb3138e08a56114d1dc58ad2267f372681ee38
Nick Berardi <nick@managedfusion.com>
2010-04-27 04:25:04 -0400

    made some real structure changes to the project, hopefully its for the best

---
+++ FluentCassandra/Actions/GetColumn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentCassandra.Types;
+using Apache.Cassandra;
+
+namespace FluentCassandra.Actions
+{
+	public class GetColumn : ColumnFamilyAction<IFluentColumn>
+	{
+		/*
+		 * ColumnOrSuperColumn get(keyspace, key, column_path, consistency_level)
+		 */
+
+		public string Key { get; set; }
+
+		public CassandraType ColumnName { get; set; }
+
+		public override bool TryExecute(CassandraColumnFamily columnFamily, out IFluentColumn result)
+		{
+			var path = new ColumnPath {
+				Column_family = columnFamily.FamilyName
+			};
+
+			if (columnFamily.SuperColumnName != null)
+				path.Super_column = columnFamily.SuperColumnName;
+
+			if (ColumnName != null)
+				path.Column = ColumnName;
+
+			var output = columnFamily.GetClient().get(
+				columnFamily.Keyspace.KeyspaceName,
+				Key,
+				path,
+				ConsistencyLevel
+			);
+
+			result = ObjectHelper.ConvertToFluentColumn(output);
+
+			return true;
+		}
+	}
+}
