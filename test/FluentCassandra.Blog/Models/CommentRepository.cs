7f297ab07893cb32348d5dd2ba1260b07c110267
Nick Berardi <nick@managedfusion.com>
2011-06-22 10:52:06 -0400

    added blog reference application and revereted default connection type to framed, since that is the default setup of cassandra

---
+++ test/FluentCassandra.Blog/Models/CommentRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Apache.Cassandra;
+using FluentCassandra.Types;
+
+namespace FluentCassandra.Blog.Models
+{
+	public class CommentRepository : CassandraRepository
+	{
+		private const string FamilyName = "Comments";
+		private CassandraColumnFamily<AsciiType> _family;
+
+		public CommentRepository()
+		{
+			Setup();
+		}
+
+		protected override void Setup()
+		{
+			base.Setup();
+
+			if (!Keyspace.ColumnFamilyExists(Server, FamilyName))
+				Keyspace.AddColumnFamily(Server, new CfDef {
+					Name = FamilyName,
+					Keyspace = KeyspaceName,
+					Column_type = "Standard",
+					Comparator_type = "AsciiType",
+					Comment = "Holds the blog posts comments"
+				});
+
+			_family = Database.GetColumnFamily<AsciiType>(FamilyName);
+		}
+
+		public FluentColumnFamily<AsciiType> Create(Guid postKey)
+		{
+			dynamic comment = _family.CreateRecord(Guid.NewGuid());
+			comment.PostKey = postKey;
+
+			return comment;
+		}
+	}
+}
---
+++ test/FluentCassandra.Blog/Models/PostRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Apache.Cassandra;
+using FluentCassandra.Types;
+
+namespace FluentCassandra.Blog.Models
+{
+	public class PostRepository : CassandraRepository
+	{
+		private const string FamilyName = "Posts";
+		private CassandraColumnFamily<AsciiType> _family;
+
+		public PostRepository()
+		{
+			Setup();
+		}
+
+		protected override void Setup()
+		{
+			base.Setup();
+
+			if (!Keyspace.ColumnFamilyExists(Server, FamilyName))
+				Keyspace.AddColumnFamily(Server, new CfDef {
+					Name = FamilyName,
+					Keyspace = KeyspaceName,
+					Column_type = "Standard",
+					Comparator_type = "AsciiType",
+					Comment = "Holds the blog posts"
+				});
+
+			_family = Database.GetColumnFamily<AsciiType>(FamilyName);
+		}
+
+		public FluentColumnFamily<AsciiType> Create()
+		{
+			var post = _family.CreateRecord(Guid.NewGuid());
+			Database.Attach(post);
+
+			return post;
+		}
+
+		public IFluentColumnFamily<AsciiType> Get(Guid postKey)
+		{
+			return _family.Get(postKey)
+				.FirstOrDefault();
+		}
+
+		public IFluentColumnFamily<AsciiType> GetBySlug(string postSlug)
+		{
+			return _family.Get(null, 1, family => family["Slug"] == postSlug)
+				.FirstOrDefault();
+		}
+
+		public IEnumerable<IFluentColumnFamily<AsciiType>> GetTop(int count)
+		{
+			return _family.Get(null, null, null, null, count);
+		}
+	}
+}
