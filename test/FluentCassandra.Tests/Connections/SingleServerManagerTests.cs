2b5cf70d8c457973cb1c9b038e5b34032f4f2724
nathannis <nathannis@gmail.com>
2013-01-24 15:27:21 -0600

    Renamed LoadBalancerServerManager to be SingleServerManager

--- test/FluentCassandra.Tests/Connections/SingleServerManagerTests.cs
+++ test/FluentCassandra.Tests/Connections/SingleServerManagerTests.cs
@@ -4,21 +4,21 @@ using Xunit;
 
 namespace FluentCassandra.Connections.Tests
 {
-    public class LoadBalancerServerManagerTests
+    public class SingleServerManagerTests
     {
         [Fact]
         public void CanGetServerAfterError()
         {
-            LoadBalancerServerManager target = new LoadBalancerServerManager(new ConnectionBuilder("Server=unit-test-1"));
+            SingleServerManager target = new SingleServerManager(new ConnectionBuilder("Server=unit-test-1"));
 
             Server original = target.Next();
 
             for (int i = 0; i < 10; i++)
             {
-                Assert.True(target.HasNext, "LoadBalancerServerManager should always have another server available.");
+                Assert.True(target.HasNext, "SingleServerManager should always have another server available.");
                 Server next = target.Next();
                 Assert.True(original.ToString().Equals(next.ToString(), StringComparison.OrdinalIgnoreCase),
-                    "LoadBalancerServerManager always returns the same server.");
+                    "SingleServerManager always returns the same server.");
                 //mark the server as failing to set up the next test iteration.
                 target.ErrorOccurred(next);
             }
