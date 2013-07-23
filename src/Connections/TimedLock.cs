6089b586cc25c9c40a406ad567bebc6859e52c24
Oleksii Mandrychenko <o.mandrychenko@inquisitive-systems.com>
2013-07-23 21:26:32 +0100

    Sort and remove unused using in /src/Connections

--- src/Connections/TimedLock.cs
+++ src/Connections/TimedLock.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Threading;
-using System.Runtime.Serialization;
 using System.Collections;
 using System.Diagnostics;
+using System.Runtime.Serialization;
+using System.Threading;
 
 namespace FluentCassandra.Connections
 {
