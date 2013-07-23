31daab3a7fc496d7b6b01540163ee7cc98b1a9e2
Oleksii Mandrychenko <o.mandrychenko@inquisitive-systems.com>
2013-07-23 21:56:43 +0100

    Added a project for integration tests
    Having both types of tests (unit and integration) in the same project it
    is difficult for new commiters to understand which tests to run. It's
    much easier to run unit tests than integration. Let's have to separate
    projects.

---
+++ test/FluentCassandra.Integration.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentCassandra.Integration.Tests")]
+[assembly: AssemblyDescription("Integration tests with real Cassandra database")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("FluentCassandra.Integration.Tests")]
+[assembly: AssemblyCompany("Managed Fusion, LLC")]
+[assembly: AssemblyCopyright("Copyright © Nick Berardi, Managed Fusion, LLC 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f0cbb9e-db27-49c1-abac-fc56d9775412")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
