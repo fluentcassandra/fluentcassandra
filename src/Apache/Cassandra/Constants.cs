1fd1b4c310b3dc61ec4b5d07dda1d939d2a2a44a
Oleksii Mandrychenko <o.mandrychenko@inquisitive-systems.com>
2013-07-23 21:23:32 +0100

    Sort and remove unused using in /src/Apache/Cassandra

--- src/Apache/Cassandra/Constants.cs
+++ src/Apache/Cassandra/Constants.cs
@@ -4,14 +4,6 @@
  * DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
  *  @generated
  */
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using FluentCassandra.Thrift;
-using FluentCassandra.Thrift.Collections;
-using System.Runtime.Serialization;
 
 namespace FluentCassandra.Apache.Cassandra
 {
