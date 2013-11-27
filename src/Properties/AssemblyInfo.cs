75d50d7fc9753b91d6434317aa762d0cfb95c270
Aaron Stannard <aaron@markedup.com>
2013-11-27 08:33:30 -0800

    adding initial rakefile and some of the supporting ruby files
    completed first cut of FluentCassandra rakefile
    
    added the ability to publish NuGet symbol packages
    
    removed the previous build / nuspec documents

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,10 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("FluentCassandra")]
 [assembly: AssemblyDescription("FluentCassandra is a .NET library for accessing Cassandra, which wraps the Thrift client library and provides a more fluent POCO interface for accessing and querying the objects in Cassandra.")]
 [assembly: AssemblyProduct("FluentCassandra")]
-[assembly: AssemblyCompany("Managed Fusion, LLC")]
-[assembly: AssemblyCopyright("Copyright © Nick Berardi, Managed Fusion, LLC 2011")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -20,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f2fac76e-0d80-4099-a64f-2686d4d99b6b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
