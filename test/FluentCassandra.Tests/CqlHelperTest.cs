0e695d643669156170f82641ed69123c156e30c2
Aaron Stannard <aaron@markedup.com>
2014-01-08 13:14:09 -0800

    added support for blob constant when working with binary data in CQL3. close #133

--- test/FluentCassandra.Tests/CqlHelperTest.cs
+++ test/FluentCassandra.Tests/CqlHelperTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace FluentCassandra
@@ -33,5 +34,20 @@ namespace FluentCassandra
             // assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void EscapeBytesForCqlTest()
+        {
+            //arrange
+            var bytes = Encoding.UTF8.GetBytes("hello world");
+            var expectedHex = "0x68656c6c6f20776f726c64"; //generated via the textAsBlob function in CQL3 shell
+
+            //act
+            var hex = CqlHelper.EscapeForCql(bytes);
+
+            //assert
+            Assert.Equal(expectedHex, hex);
+        }
+
     }
 }
